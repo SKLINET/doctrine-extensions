7db9749cc31551d15331e7e7734bc802991abcb0
Michal Sklenár <michal.sklenar@sklinet.com>
2020-09-18 20:40:58 +0200

    initial commit

---
+++ DoctrineExtensionsBundle.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace SKLINET\DoctrineExtensionsBundle;
+
+use SKLINET\DoctrineExtensionsBundle\DependencyInjection\Compiler\ValidateExtensionConfigurationPass;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+class DoctrineExtensionsBundle extends Bundle
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function build(ContainerBuilder $container)
+    {
+        $container->addCompilerPass(new ValidateExtensionConfigurationPass());
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# doctrine-extensions
+# DoctrineExtensions
+
+This bundle provides integration for
+[DoctrineExtensions](https://github.com/Atlantic18/DoctrineExtensions) in
+your Symfony Project.
+
+License: [MIT](LICENSE)
